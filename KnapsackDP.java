e40fde137c78ac220d5ecc1e3f52fcef8871e104
GEYA4106 <nedunurigeya2006@gmail.com>
2026-03-30 17:40:36 +0530

    Refactor KnapsackDP for clarity and correctness

--- KnapsackDP.java
+++ KnapsackDP.java
@@ -1,38 +1,38 @@
-//Java Progamm to implement 0/1 Knapsack using dynamic programming
-import java.util.Scanner;
-public class KnapsackDP{
-	static int knapsack(int W,int wt[],int val[],int n){
-		int dp[][]=new int[n+1][W+1];
-		for(int i=0;i<=n;i++){
-			for(int w=0;w<=W;w++){
-				if(i==0||w==0)
-					dp[i][w]=0;
-				else if(wt[i-1]<=w)
-					dp[i][w]=Math.max(val[i-1]+dp[i-1][w-wt[i-1]],dp[i-1][w]);
-				else
-					dp[i][w]=dp[i-1][w];
-			}
-		}
-		return dp[n][W];
-	}
-	public static void main(String [] args){
-		Scanner sc=new Scanner(System.in);
-		System.out.print("Enter number of items:");
-		int n=sc.nextInt();
-		int val[]=new int[n];
-		int wt[]=new int[n];
-		System.out.println("Enter values of items:");
-		for(int i=0;i<n;i++){
-			wt[i]=sc.nextInt();
-		}
-		System.out.println("Enter weights of items:");
-		for(int i=0;i<n;i++){
-			wt[i]=sc.nextInt();
-		}
-		System.out.print("Enter capacity of Knapsack:");
-		int W=sc.nextInt();
-		int result=knapsack(W,wt,val,n);
-		System.out.println("Maximum Profit="+result);
-		sc.close();
-	}
+import java.util.*;
+public class KnapsackDP {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of items:");
+        int n = sc.nextInt();
+        int[] wt = new int[n];
+        int[] val = new int[n];
+        System.out.println("Enter weights:");
+        for (int i = 0; i < n; i++) {
+            wt[i] = sc.nextInt();
+        }
+        System.out.println("Enter values:");
+        for (int i = 0; i < n; i++) {
+            val[i] = sc.nextInt();
+        }
+        System.out.println("Enter capacity of knapsack:");
+        int W = sc.nextInt();
+        int[][] dp = new int[n + 1][W + 1];
+        for (int i = 0; i <= n; i++) {
+            for (int w = 0; w <= W; w++) {
+                if (i == 0 || w == 0) {
+                    dp[i][w] = 0;
+                }
+                else if (wt[i - 1] <= w) {
+                    dp[i][w] = Math.max(
+                        val[i - 1] + dp[i - 1][w - wt[i - 1]],
+                        dp[i - 1][w]
+                    );
+                }
+                else {
+                    dp[i][w] = dp[i - 1][w];
+                }
+            }
+        }
+        System.out.println("Maximum Profit: " + dp[n][W]);
+    }
 }
