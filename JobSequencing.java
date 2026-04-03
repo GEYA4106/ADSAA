aaec3e01d3e2b9d0ee9ffd1d228598bdd7a9e197
GEYA4106 <nedunurigeya2006@gmail.com>
2026-04-03 22:26:42 +0530

    Update JobSequencing.java

--- JobSequencing.java
+++ JobSequencing.java
@@ -1,47 +1,59 @@
 import java.util.*;
-class Job {
-    int id, deadline, profit;
-    Job(int id, int deadline, int profit) {
-        this.id = id;
-        this.deadline = deadline;
-        this.profit = profit;
-    }
+class JobSequencing {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);  
+    System.out.print("Enter number of jobs: ");  
+    int n = sc.nextInt();  
+    int id[] = new int[n];  
+    int deadline[] = new int[n];  
+    int profit[] = new int[n];  
+    for (int i = 0; i < n; i++) {  
+        id[i] = i + 1;  
+        System.out.print("Enter deadline and profit for Job " + id[i] + ": ");  
+        deadline[i] = sc.nextInt();  
+        profit[i] = sc.nextInt();  
+    }    
+    for (int i = 0; i < n - 1; i++) {  
+        for (int j = 0; j < n - i - 1; j++) {  
+            if (profit[j] < profit[j + 1]) {  
+                int temp = profit[j];  
+                profit[j] = profit[j + 1];  
+                profit[j + 1] = temp;  
+
+                temp = deadline[j];  
+                deadline[j] = deadline[j + 1];  
+                deadline[j + 1] = temp;  
+
+                temp = id[j];  
+                id[j] = id[j + 1];  
+                id[j + 1] = temp;  
+            }  
+        }  
+    }  
+    int max = 0;  
+    for (int i = 0; i < n; i++) {  
+        if (deadline[i] > max)  
+            max = deadline[i];  
+    }  
+    int slot[] = new int[max];  
+    Arrays.fill(slot, -1);  
+    int totalProfit = 0;  
+    for (int i = 0; i < n; i++) {  
+        for (int j = deadline[i] - 1; j >= 0; j--) {  
+            if (slot[j] == -1) {  
+                slot[j] = id[i];  
+                totalProfit += profit[i];  
+                break;  
+            }  
+        }  
+    }  
+    System.out.print("Selected Jobs: ");  
+    for (int i = 0; i < max; i++) {  
+        if (slot[i] != -1)  
+            System.out.print("Job" + slot[i] + " ");  
+    }  
+    System.out.println("\nTotal Profit: " + totalProfit);  
+    sc.close();  
 }
-public class JobSequencing {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number of jobs:");
-        int n = sc.nextInt();
-        Job[] jobs = new Job[n];
-        System.out.println("Enter deadline and profit for each job:");
-        for (int i = 0; i < n; i++) {
-            int d = sc.nextInt();
-            int p = sc.nextInt();
-            jobs[i] = new Job(i + 1, d, p);
-        }
-        Arrays.sort(jobs, (a, b) -> b.profit - a.profit);
-        int maxDeadline = 0;
-        for (Job j : jobs) {
-            maxDeadline = Math.max(maxDeadline, j.deadline);
-        }
-        int[] slot = new int[maxDeadline + 1];
-        Arrays.fill(slot, -1);
-        int totalProfit = 0;
-        for (Job j : jobs) {
-            for (int k = j.deadline; k > 0; k--) {
-                if (slot[k] == -1) {
-                    slot[k] = j.id;
-                    totalProfit += j.profit;
-                    break;
-                }
-            }
-        }
-        System.out.println("Selected Jobs:");
-        for (int i = 1; i <= maxDeadline; i++) {
-            if (slot[i] != -1) {
-                System.out.print("J" + slot[i] + " ");
-            }
-        }
-        System.out.println("\nTotal Profit: " + totalProfit);
-    }
+
 }
