44c7ea2e164be320106cf948b8c9de9610257897
GEYA4106 <nedunurigeya2006@gmail.com>
2026-04-03 19:19:12 +0530

    Update DFSAdjList.java

--- DFSAdjList.java
+++ DFSAdjList.java
@@ -1,37 +1,46 @@
 import java.util.*;
 class DFSAdjList {
-    private int v;
-    private List<List<Integer>> adj;
+    private int n;
+    private ArrayList<ArrayList<Integer>> adj;
     private boolean[] visited;
-    public DFSAdjList(int v) {
-        this.v = v;
-        visited = new boolean[v];
+    public DFSAdjList(int n) {
+        this.n = n;
         adj = new ArrayList<>();
-
-        for (int i = 0; i < v; i++) {
+        visited = new boolean[n];
+        for (int i = 0; i < n; i++) {
             adj.add(new ArrayList<>());
         }
     }
-    public void addEdge(int src, int dest) {
-        adj.get(src).add(dest);
-        adj.get(dest).add(src);
+    public void addEdge(int i, int j) {
+        adj.get(i).add(j);
+        adj.get(j).add(i); 
     }
-    public void dfs(int start) {
+    public void DFS(int start) {
         visited[start] = true;
         System.out.print(start + " ");
-        for (int node : adj.get(start)) {
-            if (!visited[node]) {
-                dfs(node);
+        for (int neighbor : adj.get(start)) {
+            if (!visited[neighbor]) {
+                DFS(neighbor);
             }
         }
     }
     public static void main(String[] args) {
-        DFSAdjList d = new DFSAdjList(5);
-        d.addEdge(0, 1);
-        d.addEdge(0, 2);
-        d.addEdge(1, 3);
-        d.addEdge(2, 4);
-        System.out.println("DFS Traversal:");
-        d.dfs(0);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of vertices:");
+        int n = sc.nextInt();
+        DFSAdjList graph = new DFSAdjList(n);
+        System.out.println("Enter number of edges:");
+        int e = sc.nextInt();
+        System.out.println("Enter edges (u v):");
+        for (int i = 0; i < e; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            graph.addEdge(u, v);
+        }
+        System.out.println("Enter the starting vertex of DFS:");
+        int start = sc.nextInt();
+        System.out.println("DFS traversal starting from vertex " + start + ":");
+        graph.DFS(start);
+        sc.close();
     }
 }
